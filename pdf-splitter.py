a687e7ebf7353ae93df3ebc14f033266069f3bbe
e-rabello <8457978+e-rabello@users.noreply.github.com>
2024-01-12 09:42:13 -0300

    Add files via upload

---
+++ pdf-splitter.py
@@ -0,0 +1,11 @@
+from PyPDF2 import PdfWriter, PdfReader
+
+inputpdf = PdfReader(open("relatorio.pdf", "rb"))
+
+for i in range(len(inputpdf.pages)):
+    output = PdfWriter()
+    output.add_page(inputpdf.pages[i])
+    with open("document-page%s.pdf" % i, "wb") as outputStream:
+        output.write(outputStream)
+
+
